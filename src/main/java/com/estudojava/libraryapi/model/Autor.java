7272c1b6255ecb244a8c837e7f796200433da9e6
Thiago <thiagopersiadm@gmail.com>
2026-01-28 12:06:29 -0300

    aula 54 e 55

--- src/main/java/com/estudojava/libraryapi/model/Autor.java
+++ src/main/java/com/estudojava/libraryapi/model/Autor.java
@@ -1,16 +1,17 @@
 package com.estudojava.libraryapi.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "autor", schema = "public")
-@Getter
-@Setter
+@Data
 public class Autor {
 
     @Id
@@ -26,5 +27,8 @@ public class Autor {
 
     @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nacionalidade;
+
+    @OneToMany(mappedBy = "autor")
+    private List<Livro> livros;
 
 }
